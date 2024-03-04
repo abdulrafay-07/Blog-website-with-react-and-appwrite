ffa2366bdea633b1b2af1fe70613d869e7fafd7f
Abdul Rafay <152396110+abdulrafay-07@users.noreply.github.com>
2024-03-04 16:01:54 +0500

    configured redux toolkit, made a store and some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth.js';
+import { login, logout } from './store/authSlice.js';
+import { Header, Footer } from './components/index.js';
+import { Outlet } from 'react-router-dom';
+
+import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}));
+      } else {
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false));
+  }, [])
   
-  return (
-    <>
-      <h1>A blog app with appwrite.</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
