9ffd55497ca2cd4823c9c21422f6b56608248b54
Abdul Rafay <152396110+abdulrafay-07@users.noreply.github.com>
2024-03-10 15:03:54 +0500

    Created a login component and used react hook form and other components to create a form

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer.jsx";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import Logout from "./Header/Logout.jsx";
+import Input from "./Input.jsx";
 
 export { Header, Footer, Container, Logo, Logout };
